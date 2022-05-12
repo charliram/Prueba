ea31b2db9a31beeca73be653e87547deb94b3ea2
charliram <83847746+charliram@users.noreply.github.com>
2022-05-12 01:24:50 -0500

    initial commit

---
+++ src/app/clientes/clientes.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClientesService } from './clientes.service';
+import { AltaClienteComponent } from './alta-cliente/alta-cliente.component';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AltaClienteComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  providers: [
+    ClientesService
+  ],
+  exports: [
+    AltaClienteComponent
+  ]
+})
+export class ClientesModule { }
